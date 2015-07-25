4ce7dcd6b43630b6ef1df8b4baf566d469f83145
bellicose100xp <chandan7@icloud.com>
2015-07-25 13:44:55 -0700

    work upload

--- www/js/script.js
+++ www/js/script.js
@@ -2,19 +2,21 @@
  * Created by HSO on 7/25/15.
  */
 angular.module('starter')
-.controller('mainController', [function () {
+    .controller('mainController', [function () {
         var mc = this;
 
         mc.billAmount = '';
         mc.numberOfPeople = 2;
         mc.evenSplit = '';
+        mc.tip = 10;
+        mc.tipAmount = '';
+        mc.splitWithTip = '';
 
         mc.calculateEvenSplit = function () {
 
-            if(mc.billAmount && mc.numberOfPeople > 0)
-
-            {
-                mc.evenSplit = mc.billAmount / mc.numberOfPeople;
+            if (mc.billAmount && mc.numberOfPeople > 0) {
+                mc.tipAmount = mc.billAmount * (mc.tip / 100);
+                mc.evenSplit = (mc.billAmount + mc.tipAmount) / mc.numberOfPeople;
             }
 
         }
