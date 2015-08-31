0d98de93696993e6d905d48c0deae65fe158a9c8
bellicose100xp <chandan7@icloud.com>
2015-08-31 14:59:14 -0700

    work

--- www/js/discount.js
+++ www/js/discount.js
@@ -16,12 +16,20 @@ angular.module('starter')
         '$http',
         function ($scope, $ionicGesture, $cordovaGeolocation, $ionicPlatform, $http) {
             var dc = this;
+            dc.price = '';
+            dc.tax = 7; //percent
+            dc.discountPercent = 20; //percent
+            dc.totalDiscount = '';
+            dc.finalPriceBeforeTax = '';
+            dc.finalPrice = '';
+            dc.finalTaxAmount = '';
+            dc.location = '';
 
             $ionicPlatform.ready(function () {
 
 
                 var watchOptions = {
-                    frequency: 1000,
+                    frequency: 5000,
                     timeout: 10000,
                     enableHighAccuracy: false // may cause errors if true
                 };
@@ -39,21 +47,27 @@ angular.module('starter')
 
                         var latlong = lat + ',' + long;
 
-                        console.log(latlong);
+                       // console.log(latlong);
 
                         var googleMapApiUrl = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&language=en&latlng=' + latlong;
 
                         $http.get(googleMapApiUrl)
                             .then(function (mapData) {
+                              //  console.log(mapData);
                                 var parsedMapData = mapData.data.results[0].address_components;
                                 var street = parsedMapData[0].long_name + ' ' + parsedMapData[1].long_name;
-                                var city = parsedMapData[2].long_name;
-                                var state= parsedMapData[5].short_name;
-                                var country = 'USA';
-                                var zipCode = parsedMapData[7].short_name;
+                                street = street.replace(/ /g, '+');
+                               // console.log(street);
+                                var cityOriginal = parsedMapData[3].long_name
+                                var city = cityOriginal.toLowerCase();
+                                var state= parsedMapData[6].short_name.toLowerCase();
+                                var zipCode = parsedMapData[8].short_name;
+                                //console.log(street, city, zipCode, state);
+                                dc.location = cityOriginal + ' ,' + state.toUpperCase();
 
                                 //http://taxratesapi.avalara.com/account
-                                var avalaraApiKey = 'zSQlGO+APfIeAUvwPV/vw6DVFzV0bZPtla/ywaQt3Y86r+VPoDhVLtNn/fAgnbGVmI/6FZdzt2edj4mVo/LtVQ==';
+                               // var avalaraApiKey = 'zSQlGO+APfIeAUvwPV/vw6DVFzV0bZPtla/ywaQt3Y86r+VPoDhVLtNn/fAgnbGVmI/6FZdzt2edj4mVo/LtVQ==';
+                                var avalaraApiKey = 'zSQlGO%2BAPfIeAUvwPV%2Fvw6DVFzV0bZPtla%2FywaQt3Y86r%2BVPoDhVLtNn%2FfAgnbGVmI%2F6FZdzt2edj4mVo%2FLtVQ%3D%3D';
                                 var avalaraUrl = 'https://taxrates.api.avalara.com:443/address?country=usa&state=' +
                                 state + '&city=' + city + '&postal=' + zipCode + '&street=' + street + '&apikey=' + avalaraApiKey;
 
@@ -62,9 +76,12 @@ angular.module('starter')
                             }, function (err) {
                                 console.log('google api error: ', err)
                             })
-                            .then(function (data) {
-                                console.log(data);
+                            .then(function (tax) {
+                               // console.log(tax);
+                                dc.tax = tax.data.totalRate;
+                                //console.log(tax.data.totalRate);
                             }, function(err){
+                                dc.location = '';
                                 console.log('error getting tax: ', err);
                             })
 
@@ -72,13 +89,7 @@ angular.module('starter')
 
             });
 
-            dc.price = '';
-            dc.tax = 9; //percent
-            dc.discountPercent = 20; //percent
-            dc.totalDiscount = '';
-            dc.finalPriceBeforeTax = '';
-            dc.finalPrice = '';
-            dc.finalTaxAmount = '';
+
 
 
             dc.calculateDiscount = function () {
